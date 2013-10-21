c624d6ae42cefaab665f79c9e44eaa3bc1ef0685
rakhound <r_asadeghi@yahoo.com>
2013-10-20 21:25:10 -0700

    1- Made some changes to task table of the database and made corresponding    changes to the code. 2- Added table for category to the database and added code for getting    categories from the server and displaying them. Also implemented    adding new categories in place. 3- Implemented the tree(hierarchical)display for categories, along with    expand/collapse functionality.

---
+++ application/controllers/api/categories.php
@@ -0,0 +1,65 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+require_once(APPPATH.'/libraries/FirePHPCore/fb.PHP');
+require APPPATH.'/libraries/REST_Controller.php';
+/**
+ * Projects API controller
+ *
+ * Validation is missing
+ */
+class Categories extends REST_Controller {
+
+	public function __construct()
+	{
+	    parent::__construct();
+
+	    $this->load->model('category_model');
+	}
+
+	public function index_get()
+	{
+		$tasks = $this->category_model->get_all();
+		$this->response($tasks);
+	}
+
+	public function edit_get($id = NULL)
+	{
+		if ( ! $id)
+		{
+			$this->response(array('status' => false, 'error_message' => 'No ID was provided.'), 400);
+		}
+
+		$this->response($this->category_model->get($id));
+	}
+
+	public function save_post($id = NULL)
+	{
+		if ($id == NULL)
+		{
+			FB::info("Inside save_post, without id)");
+			$new_id = $this->category_model->add($this->post());
+			$this->response(array('status' => true, 'id' => $new_id, 'message' => sprintf('Task #%d has been created.', $new_id)), 200);
+		}
+		else
+		{
+			FB::info("Inside save_post, id)");
+			$this->category_model->update($id, $this->post());
+			$this->response(array('status' => true, 'message' => sprintf('Task #%d has been updated.', $id)), 200);
+		}
+	}
+
+	public function remove_delete($id = NULL)
+	{
+		if ($this->category_model->delete($id))
+		{
+			$this->response(array('status' => true, 'message' => sprintf('Task #%d has been deleted.', $id)), 200);
+		}
+		else
+		{
+			$this->response(array('status' => false, 'error_message' => 'This Task does not exist!'), 404);
+		}
+	}
+
+}
+
+/* End of file categories.php */
+/* Location: ./application/controllers/api/categories.php */
--- application/models/category_model.php
+++ application/models/category_model.php
@@ -1,7 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Task_model extends CI_Model
+
+class Category_model extends CI_Model
 {
-    public $table = 'task';
+    public $table = 'category';
 
     public function get_all()
     {
@@ -10,12 +11,12 @@ class Task_model extends CI_Model
     
     public function get($id)
     {
-        return $this->db->where('TaskId', $id)->get($this->table)->row();
+        return $this->db->where('CategoryId', $id)->get($this->table)->row();
     }
   
     public function add($data)
     {
-		FB::info("inside add method in task_model"); 
+		FB::info("inside add method in category_model"); 
 		FB::info($data); 
         $this->db->insert($this->table, $data);
 		FB::info($this->db->insert_id()); 
@@ -24,16 +25,16 @@ class Task_model extends CI_Model
 
     public function update($id, $data)
     {
-        return $this->db->where('TaskId', $id)->update($this->table, $data);
+        return $this->db->where('CategoryId', $id)->update($this->table, $data);
     }
 
     public function delete($id)
     {
-        $this->db->where('TaskId', $id)->delete($this->table);
+        $this->db->where('CategoryId', $id)->delete($this->table);
         return $this->db->affected_rows();
     }
 
 }
 
-/* End of file task_model.php */
-/* Location: ./application/models/task_model.php */
+/* End of file category_model.php */
+/* Location: ./application/models/category_model.php */
