113e00c75d13368e491d732cefea211cd174e11f
leahmarsh <leah@thejibe.com>
2013-10-23 18:09:44 -0700

    Remove ng folder from application folder.

--- application/ng/js/mmenu/README.md
+++
@@ -1,5 +0,0 @@
-jquery.mmenu
-===============
-
-A jQuery plugin for creating slick, app look-alike sliding menus for you mobile website.
-For examples and the complete documentation, visit http://mmenu.frebsite.nl
--- application/ng/js/script.js
+++
@@ -1,9 +0,0 @@
-// Mmenu configuration
-$(function() {
-	$('a#open-icon').click(function( e ) {
-		e.stopImmediatePropagation();
-		e.preventDefault();
-		$('nav#menu').trigger( 'toggle.mm' );
-	});
-	$('nav#menu').mmenu();
-});
