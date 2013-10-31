c5b268689279451f2f4275b85461c62b524ef083
leahmarsh <leah@thejibe.com>
2013-10-30 18:17:50 -0700

    Update database to accept new fields.

--- task_management.sql
+++ task_management.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 3.5.7
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 21, 2013 at 06:40 AM
--- Server version: 5.5.32
--- PHP Version: 5.4.19
+-- Host: localhost
+-- Generation Time: Oct 30, 2013 at 06:16 PM
+-- Server version: 5.5.29
+-- PHP Version: 5.3.20
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `task_management`
 --
-CREATE DATABASE IF NOT EXISTS `task_management` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `task_management`;
 
 -- --------------------------------------------------------
 
@@ -28,12 +26,12 @@ USE `task_management`;
 -- Table structure for table `category`
 --
 
-CREATE TABLE IF NOT EXISTS `category` (
+CREATE TABLE `category` (
   `CategoryId` int(11) NOT NULL AUTO_INCREMENT,
   `ParentId` int(11) NOT NULL,
   `CategoryTitle` text NOT NULL,
   PRIMARY KEY (`CategoryId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `category`
@@ -57,28 +55,31 @@ INSERT INTO `category` (`CategoryId`, `ParentId`, `CategoryTitle`) VALUES
 -- Table structure for table `task`
 --
 
-CREATE TABLE IF NOT EXISTS `task` (
+CREATE TABLE `task` (
   `TaskId` int(11) NOT NULL AUTO_INCREMENT,
   `TaskTitle` tinytext NOT NULL,
   `StartDate` datetime NOT NULL,
   `DueDate` datetime NOT NULL,
+  `BudgetTime` tinyint(4) NOT NULL,
   `SpentTime` tinyint(4) NOT NULL,
   `Status` enum('Open','Closed','Suspended','') NOT NULL DEFAULT 'Open',
   `Notes` text NOT NULL,
+  `TaskCat` varchar(128) NOT NULL,
   PRIMARY KEY (`TaskId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 --
 -- Dumping data for table `task`
 --
 
-INSERT INTO `task` (`TaskId`, `TaskTitle`, `StartDate`, `DueDate`, `SpentTime`, `Status`, `Notes`) VALUES
-(1, 'Task number one', '0000-00-00 00:00:00', '2013-10-31 09:00:00', 0, 'Open', 'This is an important task'),
-(2, 'Task number two', '0000-00-00 00:00:00', '2013-10-19 10:12:18', 0, 'Open', 'This should be taken seriously'),
-(3, 'Task number three', '0000-00-00 00:00:00', '2013-10-16 04:09:09', 0, 'Open', 'This is not as important.'),
-(4, 'Task number four', '0000-00-00 00:00:00', '2013-11-13 17:30:00', 0, 'Open', 'This is optional.'),
-(6, 'Task number two', '0000-00-00 00:00:00', '2013-10-19 10:12:18', 0, 'Open', 'This should be taken seriously'),
-(7, 'Task number one', '0000-00-00 00:00:00', '2013-10-31 09:00:00', 0, 'Open', 'This is an important task');
+INSERT INTO `task` (`TaskId`, `TaskTitle`, `StartDate`, `DueDate`, `BudgetTime`, `SpentTime`, `Status`, `Notes`, `TaskCat`) VALUES
+(1, 'Task number one', '0000-00-00 00:00:00', '2013-10-31 09:00:00', 0, 0, 'Open', 'This is an important task', ''),
+(2, 'Task number two', '0000-00-00 00:00:00', '2013-10-19 10:12:18', 0, 0, 'Open', 'This should be taken seriously', ''),
+(3, 'Task number three', '0000-00-00 00:00:00', '2013-10-16 04:09:09', 0, 0, 'Open', 'This is not as important.', ''),
+(4, 'Task number four', '0000-00-00 00:00:00', '2013-11-13 17:30:00', 0, 0, 'Open', 'This is optional.', ''),
+(6, 'Task number two', '0000-00-00 00:00:00', '2013-10-19 10:12:18', 0, 0, 'Open', 'This should be taken seriously', ''),
+(7, 'Task number one', '0000-00-00 00:00:00', '2013-10-31 09:00:00', 0, 0, 'Open', 'This is an important task', ''),
+(21, 'New Task', '2013-10-31 09:00:00', '2013-10-31 01:15:07', 5, 2, 'Open', 'This is what I want to do...', 'Root2');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
